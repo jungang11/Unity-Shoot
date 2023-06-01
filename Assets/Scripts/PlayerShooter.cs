42fd084170cf7b14167b323667e3b74b2cb21bff
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-06-01 21:15:58 +0900

    feat : Weapon, Gun, ObejctPool

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 using UnityEngine.InputSystem;
 
 public class PlayerShooter : MonoBehaviour
 {
+    [SerializeField] private float reloadtime;
+    [SerializeField] Rig aimRig;
+    [SerializeField] WeaponHolder weaponHolder;
+
     private Animator anim;
+    private bool isReloading;
 
     private void Awake()
     {
@@ -14,11 +20,33 @@ public class PlayerShooter : MonoBehaviour
 
     private void OnReload(InputValue value)
     {
+        if (isReloading)
+            return;
+
+        StartCoroutine(ReloadRoutine());
+    }
+
+    IEnumerator ReloadRoutine()
+    {
         anim.SetTrigger("Reload");
+        isReloading = true;
+        aimRig.weight = 0f;
+        yield return new WaitForSeconds(reloadtime);
+        isReloading=false;
+        aimRig.weight = 1f;
+    }
+
+    public void Fire()
+    {
+        weaponHolder.Fire();
+        anim.SetTrigger("Fire");
     }
 
     private void OnFire(InputValue value)
     {
-        anim.SetTrigger("Fire");
+        if (isReloading)
+            return;
+
+        Fire();
     }
 }
